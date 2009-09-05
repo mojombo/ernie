834a7f4dbd7017b99e96d21fbf7c2ac82da454ea
Tom Preston-Werner <tom@mojombo.com>
2009-09-05 01:35:38 -0700

    add load tester script

--- test/load.rb
+++ test/load.rb
@@ -1,14 +1,18 @@
 require 'bertrpc'
 
+$stdout.sync = true
+
 threads = []
 svc = BERTRPC::Service.new('localhost', 8000)
 
-5.times do
+8.times do
   threads << Thread.new do
     i = 0
-    100.times { i += svc.call.calc.add(1, 2) }
-    print "#{i}\n"
+    10.times { i += svc.call.calc.add(1, 2); print '.'; $stdout.flush }
+    print "(#{i})"
   end
 end
 
 threads.each { |t| t.join }
+
+puts
