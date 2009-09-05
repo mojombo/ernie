766c22c3292eea2f879c39727ce8d051cc48bee3
Tom Preston-Werner <tom@mojombo.com>
2009-09-05 01:35:02 -0700

    add slowcalc example

--- examples/calc.rb
+++ examples/calc.rb
@@ -1,8 +1,17 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'ernie'
 
+# Just about the easiest example I could thing of.
 mod(:calc) do
   fun(:add) do |a, b|
+    a + b
+  end
+end
+
+# Useful for tests that need to simulate longer running functions.
+mod(:slowcalc) do
+  fun(:add) do |a, b|
+    sleep(rand * 2)
     a + b
   end
 end
