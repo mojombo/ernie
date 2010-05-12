171332d67db9806d769372f1f0c68d3396fbf556
Tom Preston-Werner <tom@mojombo.com>
2010-05-11 17:16:34 -0700

    increase erlang tcp backlog from 5 to 128

--- elib/ernie_server.erl
+++ elib/ernie_server.erl
@@ -115,7 +115,7 @@ try_listen(Port, 0) ->
   error_logger:error_msg("Could not listen on port ~p~n", [Port]),
   {error, "Could not listen on port"};
 try_listen(Port, Times) ->
-  Res = gen_tcp:listen(Port, [binary, {packet, 4}, {active, false}, {reuseaddr, true}]),
+  Res = gen_tcp:listen(Port, [binary, {packet, 4}, {active, false}, {reuseaddr, true}, {backlog, 128}]),
   case Res of
     {ok, LSock} ->
       error_logger:info_msg("Listening on port ~p~n", [Port]),
